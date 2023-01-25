258ccf9550f47273ab5555456d7f569830cb67c5
91ahmed <ahmedh12491@gmail.com>
2023-01-25 20:27:35 +0200

    new update

--- config.js
+++ config.js
@@ -1,3 +1,26 @@
+const config = new Map()
+
+// Menu Links text
+config.set('home', 'Who I am?')
+config.set('resume', 'Resume')
+config.set('portfolio', 'Portfolio')
+config.set('contact', 'Contact')
+
+
+
+
+
+$(function(){
+	// Menu Links text
+	$('#about').text(config.get('home'))
+	$('#resume').text(config.get('resume'))
+	$('#portfolio').text(config.get('portfolio'))
+	$('#contact').text(config.get('contact'))
+})
+
+//alert(link)
+
+/*
 if (location.hostname === "localhost" || location.hostname === "127.0.0.1" || location.hostname == '')
 {
     // Local host
@@ -6,3 +29,4 @@ else
 {
     // Live host
 }
+*/
