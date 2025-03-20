862b51a0faeae30687f452bee30d2202b315da71
91ahmed <ahmedh12491@gmail.com>
2025-03-19 22:32:14 -0700

    update the top menu

--- projects/shoebill/web-assets/js/main.js
+++ projects/shoebill/web-assets/js/main.js
@@ -3,4 +3,16 @@ $(document).ready(function () {
 	// Initialize Select Picker
 	$('.selectpicker').selectpicker()
 
+	// Initialize WOW animation
+	new WOW().init();
+
+	// Dropdown list
+	$('.list-btn').on('click', function () {
+		// $('.link-drop-ul').removeClass('active-li-list')
+		// if ($(this).find('.link-drop-ul').hasClass('active-li-list')) {
+			
+		// }
+		$(this).find('.link-drop-ul').toggleClass('active-li-list')
+	})	
+
 })
