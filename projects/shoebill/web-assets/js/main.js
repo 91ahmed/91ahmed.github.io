0a1f12651590caae7c0e7a3fd671347fc6b39ae3
91ahmed <ahmedh12491@gmail.com>
2025-03-20 21:11:44 -0700

    remove the list animation

--- projects/shoebill/web-assets/js/main.js
+++ projects/shoebill/web-assets/js/main.js
@@ -6,17 +6,4 @@ $(document).ready(function () {
 	// Initialize WOW animation
 	new WOW().init();
 
-	// Dropdown list
-	$('.list-l').on('click', function () {
-		if ($('.list-drop-ul').hasClass('active-li-list')) {
-			$('.list-drop-ul').removeClass('active-li-list')
-		} else {
-			$(this).find('.list-drop-ul').addClass('active-li-list')
-		}
-	})
-
-	$('.list-l ul li').on('focusout', function () {
-		$('.list-drop-ul').removeClass('active-li-list')
-	})
-
 })
