c1e0aae05e4f84a37acf56d91f056ae8cffc955f
91ahmed <ahmedh12491@gmail.com>
2025-09-09 21:00:25 -0700

    Create new post

--- assets/js/posts.js
+++ assets/js/posts.js
@@ -1,13 +1,14 @@
 const posts = [
 	{
 		id: 1,
-		title: 'التعامل مع الاعداد في لغة الجافاسكريبت (Octal, Hex, Binary)',
+		title: ' تمثيل الاعداد في لغة الجافاسكريبت (Octal, Hex, Binary)',
 		category: 'javascript',
 		path: 'post/js/js_01.html',
 		time: 'Oct 16, 2021',
 		created_year: '2021',
 		created_month: 'Oct',
-		created_day: '16'
+		created_day: '16',
+		author: 'Ahmed Hassan'
 	},	
 	{
 		id: 2,
@@ -17,6 +18,26 @@ const posts = [
 		time: 'Oct 16, 2021',
 		created_year: '2021',
 		created_month: 'Oct',
-		created_day: '16'
+		created_day: '16',
+		author: 'Ahmed Hassan'
 	}
 ]
+
+let currentUrl = window.location.href
+currentUrl = currentUrl.split('/')
+
+let urlTitle = currentUrl[currentUrl.length-1]
+
+for (let p of posts) {
+	let postTitle = p.path.split('/')
+	postTitle = postTitle[postTitle.length-1]
+	
+	if (postTitle === urlTitle) {
+		document.getElementsByTagName('title')[0].innerHTML = p.title
+		document.getElementById('printTitle').innerHTML = p.title
+		document.getElementById('printAuthor').innerHTML = p.author
+		document.getElementById('printDate').innerHTML = p.time
+		document.getElementById('printCategory').innerHTML = p.category
+		break
+	}
+}
