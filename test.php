53c0b1a0dfa277a3e8033727fdb9b2a7cd8f217a
91ahmed <ahmedh12491@gmail.com>
2025-09-25 01:36:56 -0700

    update oop_01.html

--- test.php
+++ test.php
@@ -1,3 +1,24 @@
 <?php 
+	class Creatures { 
+		public function makeSound() { 
+			return "Some sound"; 
+		} 
+	} 
 
+	class Shobill extends Creatures { 
+		public function makeSound() { 
+			return "Clattering"; 
+		} 
+	} 
+
+	class Bear extends Creatures { 
+		public function makeSound() { 
+			return "Growl"; 
+		} 
+	} 
+
+	$creatures = [new Shobill(), new Bear()]; 
+	foreach($creatures as $creature) { 
+		echo $creature->makeSound() . "<br>"; 
+	}
 ?> 
